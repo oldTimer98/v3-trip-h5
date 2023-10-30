796b4df2219fc36ac6989c9b11df280304202f11
oldTimer <1316767658@qq.com>
2023-10-30 17:41:08 +0800

    内容导航

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="detail top-page">detail:{{ route.params.id }}</div>
+  <div class="detail top-page">
+    <van-nav-bar title="房屋详情" left-text="旅途" left-arrow @click-left="onClickLeft" />
+    我是内容
+  </div>
 </template>
 
 <script setup>
-import { useRoute } from "vue-router"
+import { useRoute, useRouter } from "vue-router"
 
 const route = useRoute()
+const router = useRouter()
+const onClickLeft = () => {
+  router.back()
+}
 </script>
 
 <style lang="scss" scoped></style>
